655acef587e745666d2cafc9ec555c1d1191256e
Ricardo Porras <ricardoporras@outlook.com>
2021-11-16 20:26:23 -0700

    Added functions to make code easier to read

--- app1.js
+++ app1.js
@@ -1,17 +1,20 @@
-let firstName = prompt('Hello, what is your first name?');
-let lastName = prompt('Hello, what is your last name?');
-let confirmation = confirm('Are you sure?');
-if (confirmation == false) {
+
+function askName() {
     let firstName = prompt('Hello, what is your first name?');
-    let lastName = prompt('Hello, what is your last name?');
-    document.write('<h2> Hello ' + firstName + ' ' + lastName + '!</h2>');
+    let lastName = prompt('And your last name?');
+    let confirmation = confirm('Are you sure?');
+    if (confirmation == false) {
+        let firstName = prompt('Hello, what is your first name?');
+        let lastName = prompt('And your last name?');
+        document.write('<h2> Hello ' + firstName + ' ' + lastName + '!</h2>');
+    }
+    else {
+        document.write('<h2> Hello ' + firstName + ' ' + lastName + '!</h2>');
+    }
 }
-else {
-    document.write('<h2> Hello ' + firstName + ' ' + lastName + '!</h2>');
+function askForCause() {
+    let question = confirm('Would you like to help our cause and fight Malaria?');
+    if (question == true) {
+        document.write('<h2>Thank you for your desire to help, please read the article to learn more.</h2>')
+    }
 }
-
-let question = prompt('Would you like to help our cause and fight Malaria?');
-if (question == 'yes') {
-    document.write('<h2>Thank you for your desire to help, please read the article to learn more.</h2>')
-}
-
